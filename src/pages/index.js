a0aac24612b7b4a3a9f696100bdf3463e16a40e5
Sung Woo Oh <sungbr95@gmail.com>
2021-08-22 21:36:43 -0400

    applied eslint&prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
-export default function Home() {
-  return <div><h1>Music Box</h1><a href="https://github.com/mbox-contributors/mbox">Learn more</a></div>
+const Home = () => {
+  return (
+    <div>
+      <h1>Music Box</h1>
+      <a href="https://github.com/mbox-contributors/mbox">Learn more</a>
+    </div>
+  )
 }
+
+export default Home
